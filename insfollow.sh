77423aeb583d301ba646858ae24e5bc3664d7639
exaon18 <69327021+exaon18@users.noreply.github.com>
2023-12-02 15:07:26 +0300

    Update insfollow.sh

--- insfollow.sh
+++ insfollow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # insfollow v2.0
-# recoded by: Termux Professor
-# github.com/termuxprofessor/insfollow
+# reverse engineered: exaon
+# github.com/exaon18/insfollow
 # If you use any part from this code, give me the credits, please, read the License
 
 clear
